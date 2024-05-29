fd5ba5a6ebbe2e2f2d543cfb185cc7d6ad8f0349
Sarthak Chandel <60472511+SChandel-cmd@users.noreply.github.com>
2024-05-29 17:35:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+document.addEventListener('mousemove', function(e) {
+    const spark = document.createElement('div');
+    spark.className = 'spark';
+    spark.style.left = `${e.clientX}px`;
+    spark.style.top = `${e.clientY}px`;
+    document.body.appendChild(spark);
+    // Remove the spark element after animation completes
+    setTimeout(() => {
+        document.body.removeChild(spark);
+    }, 1000); // Adjust duration as needed
+});
